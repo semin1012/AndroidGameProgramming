815d210ac69390e174e0d09b9812983343795a14
semin1012 <semin1012@gmail.com>
2023-03-14 15:05:04 +0900

    change Structure
    구조를 변경함. ImageView 와 TextView 를 멤버변수로 빼내어 더욱 용이하게 변경하도록 변경.
    1 / 5 처럼 페이지를 나타내는 것의 최댓값이 자유롭게 변경되도록 변경하였다.

--- Study/02/app/src/main/java/com/tukorea2020182032/imageswiture/MainActivity.java
+++ Study/02/app/src/main/java/com/tukorea2020182032/imageswiture/MainActivity.java
@@ -11,8 +11,12 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getCanonicalName();
+
+    private ImageView mainImageView;
+    private TextView pageTextView;
+
     int page = 1;
-    private static int[] resIds = new int[]{
+    private static final int[] IMG_RES_IDS = new int[]{
             // Build 할 때 생성 되어서 한 번도 안 바뀔 것이니 private 를 붙이고,
             // 변하지 않도록 static 을 붙이고, 멤버 함수 바깥에 위치하도록 한다. (최초 한 번만 생성)
             R.mipmap.cat_1,
@@ -27,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainImageView = findViewById(R.id.mainImageView);
+        pageTextView = findViewById(R.id.PageTextView);
+        pageTextView.setText(page + " / " + IMG_RES_IDS.length);
     }
 
     public void onButtonPrev(View view) {
@@ -41,13 +48,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setPage(int page) {
-        if (page < 1 || page > resIds.length ) return;
-        int resId = resIds[page - 1];
-        ImageView iv = findViewById(R.id.mainImageView);
-        iv.setImageResource(R.mipmap.cat_2);
-        iv.setImageResource(resId);
-        TextView tv = findViewById(R.id.PageTextView);
-        tv.setText(page + " / " + resIds.length);
+        if (page < 1 || page > IMG_RES_IDS.length ) return;
+        int resId = IMG_RES_IDS[page - 1];
+        mainImageView.setImageResource(R.mipmap.cat_2);
+        mainImageView.setImageResource(resId);
+        pageTextView.setText(page + " / " + IMG_RES_IDS.length);
         this.page = page;
     }
 }
