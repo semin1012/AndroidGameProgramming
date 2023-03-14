ff20b956e09f13c0d896f567cb464aa75e502865
semin1012 <semin1012@gmail.com>
2023-03-14 14:44:09 +0900

    change onClickPrev, Next Function
    클릭 함수들 내 나름대로 변경.
    너무 금방 만든 것이라 얼레벌레.

--- Study/02/app/src/main/java/com/tukorea2020182032/imageswiture/MainActivity.java
+++ Study/02/app/src/main/java/com/tukorea2020182032/imageswiture/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "MainActivity";
+    public static final String TAG = MainActivity.class.getCanonicalName();
+    int indexImage = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +20,58 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonPrev(View view) {
         Log.d(TAG, "Prev Pressed");
+
+        if ( indexImage > 1 ) {
+            indexImage -= 1;
+        }
+
+        ImageView iv = findViewById(R.id.mainImageView);;
+
+        switch(indexImage){
+            case 1:
+                iv.setImageResource(R.mipmap.cat_1);
+                break;
+            case 2:
+                iv.setImageResource(R.mipmap.cat_2);
+                break;
+            case 3:
+                iv.setImageResource(R.mipmap.cat_3);
+                break;
+            case 4:
+                iv.setImageResource(R.mipmap.cat_4);
+                break;
+            case 5:
+                iv.setImageResource(R.mipmap.cat_5);
+                break;
+        }
     }
 
     public void onButtonNext(View view) {
         Log.d(TAG, "Next Pressed");
+
+        if ( indexImage < 5 ) {
+            indexImage += 1;
+        }
+
+        ImageView iv = findViewById(R.id.mainImageView);;
+
+        switch(indexImage){
+            case 1:
+                iv.setImageResource(R.mipmap.cat_1);
+                break;
+            case 2:
+                iv.setImageResource(R.mipmap.cat_2);
+                break;
+            case 3:
+                iv.setImageResource(R.mipmap.cat_3);
+                break;
+            case 4:
+                iv.setImageResource(R.mipmap.cat_4);
+                break;
+            case 5:
+                iv.setImageResource(R.mipmap.cat_5);
+                break;
+        }
     }
 
 }
