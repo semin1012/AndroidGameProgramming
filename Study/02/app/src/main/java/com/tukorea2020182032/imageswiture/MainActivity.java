0e0b88a93307c2903ed40d5699d6a873bc943c14
semin1012 <semin1012@gmail.com>
2023-03-14 14:56:49 +0900

    Implement OnclickButton Func
    클릭 버튼이 눌렸을 때
    1) 이미지 변경 (완료)
    2) 텍스트 변경 (완료)
    
    private static final 붙이는 이유 잘 알아두고 유용하게 사용하기
    - 메모리 내에 계속 위치하게 됨 (장점이나 단점)
    - 변하지 않음, 최초 1 회에만 넣어짐, 변경 되지 않도록 함
    - final 은 더 이상 바뀌지 않는다고 명시하는 역할이다.
    - 변하지 않는 값은 대문자로 변수명을 짓는다.

--- Study/02/app/src/main/java/com/tukorea2020182032/imageswiture/MainActivity.java
+++ Study/02/app/src/main/java/com/tukorea2020182032/imageswiture/MainActivity.java
@@ -6,11 +6,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getCanonicalName();
-    int indexImage = 1;
+    int page = 1;
+    private static int[] resIds = new int[]{
+            // Build 할 때 생성 되어서 한 번도 안 바뀔 것이니 private 를 붙이고,
+            // 변하지 않도록 static 을 붙이고, 멤버 함수 바깥에 위치하도록 한다. (최초 한 번만 생성)
+            R.mipmap.cat_1,
+            R.mipmap.cat_2,
+            R.mipmap.cat_3,
+            R.mipmap.cat_4,
+            R.mipmap.cat_5,
+            R.mipmap.cat_6,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,57 +32,22 @@ public class MainActivity extends AppCompatActivity {
     public void onButtonPrev(View view) {
         Log.d(TAG, "Prev Pressed");
 
-        if ( indexImage > 1 ) {
-            indexImage -= 1;
-        }
-
-        ImageView iv = findViewById(R.id.mainImageView);;
-
-        switch(indexImage){
-            case 1:
-                iv.setImageResource(R.mipmap.cat_1);
-                break;
-            case 2:
-                iv.setImageResource(R.mipmap.cat_2);
-                break;
-            case 3:
-                iv.setImageResource(R.mipmap.cat_3);
-                break;
-            case 4:
-                iv.setImageResource(R.mipmap.cat_4);
-                break;
-            case 5:
-                iv.setImageResource(R.mipmap.cat_5);
-                break;
-        }
+        setPage(page - 1);
     }
 
     public void onButtonNext(View view) {
         Log.d(TAG, "Next Pressed");
-
-        if ( indexImage < 5 ) {
-            indexImage += 1;
-        }
-
-        ImageView iv = findViewById(R.id.mainImageView);;
-
-        switch(indexImage){
-            case 1:
-                iv.setImageResource(R.mipmap.cat_1);
-                break;
-            case 2:
-                iv.setImageResource(R.mipmap.cat_2);
-                break;
-            case 3:
-                iv.setImageResource(R.mipmap.cat_3);
-                break;
-            case 4:
-                iv.setImageResource(R.mipmap.cat_4);
-                break;
-            case 5:
-                iv.setImageResource(R.mipmap.cat_5);
-                break;
-        }
+        setPage(page + 1);
     }
 
+    private void setPage(int page) {
+        if (page < 1 || page > resIds.length ) return;
+        int resId = resIds[page - 1];
+        ImageView iv = findViewById(R.id.mainImageView);
+        iv.setImageResource(R.mipmap.cat_2);
+        iv.setImageResource(resId);
+        TextView tv = findViewById(R.id.PageTextView);
+        tv.setText(page + " / " + resIds.length);
+        this.page = page;
+    }
 }
