87b77c59694b8f45a267ee2667e34b9851feb1e7
semin1012 <semin1012@gmail.com>
2023-03-14 14:11:49 +0900

    Add onClickButton & Debug Log
    클릭 시의 함수를 추가하고 디버깅 로그 출력하는 법을 배웠다.

--- Study/02/app/src/main/java/com/tukorea2020182032/imageswiture/MainActivity.java
+++ Study/02/app/src/main/java/com/tukorea2020182032/imageswiture/MainActivity.java
@@ -3,6 +3,8 @@ package com.tukorea2020182032.imageswiture;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onButtonPrev(View view) {
+        Log.d("MainActivity", "Prev Pressed");
+    }
+
+    public void onButtonNext(View view) {
+        Log.d("MainActivity", "Next Pressed");
+    }
+
 }
