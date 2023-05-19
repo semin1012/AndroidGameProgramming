8f8d973084f748de4bd70a77328674ca448b0adb
semin1012 <semin1012@gmail.com>
2023-05-19 11:29:18 +0900

    add title activity

---
+++ DontStop/app/src/main/java/kr/ac/tukorea/ge/DontStop/DontStop/app/TitleActivity.java
@@ -0,0 +1,16 @@
+package kr.ac.tukorea.ge.DontStop.DontStop.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import kr.ac.tukorea.ge.DontStop.DontStop.R;
+
+public class TitleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+    }
+}
